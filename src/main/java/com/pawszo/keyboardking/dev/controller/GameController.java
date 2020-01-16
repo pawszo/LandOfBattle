007760a4ed106a63474ca8ac9a7aa12e4527a1f2
pawszo <paulszoltysek@gmail.com>
2020-01-16 01:52:00 +0100

    commit working webside, missing user panel and minor bugs

--- src/main/java/com/pawszo/keyboardking/dev/controller/GameController.java
+++ src/main/java/com/pawszo/keyboardking/dev/controller/GameController.java
@@ -6,11 +6,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping
 @Controller
+@CrossOrigin
 public class GameController {
 
     @GetMapping(value = {"/shooter", "/starter"})
